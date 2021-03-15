193ff2d243f59a0a7825d8c3a4a60bebd3cb3daa
akoul <akoul@DESKTOP-PAS2TGA>
2021-03-15 12:56:11 +0530

    framework

---
+++ gradedossier/src/test/java/Fusemint/gradedossier/contacttest.java
@@ -0,0 +1,28 @@
+package Fusemint.gradedossier;
+
+import org.testng.annotations.Test;
+
+public class contacttest {
+	
+	@Test
+	public void createcontacttest(){
+		
+		System.out.println("created");
+		
+	}
+	
+	@Test
+	public void editcontacttest(){
+		
+		System.out.println("edited");
+		
+	}
+	
+	@Test
+	public void delcontacttest(){
+		
+		System.out.println("deleted");
+		
+	}
+	
+}
